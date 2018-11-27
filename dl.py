becb0f3e4aa9769284e8006674ca6121cd754f07
Andrzej Sikorski <andrzejstudia@gmail.com>
2018-11-28 00:30:43 +0100

    Upload

--- dl.py
+++ dl.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, session, redirect, render_template, send_from_directory, send_file
+from flask import Flask, request, session, redirect, render_template, send_from_directory, send_file, url_for, abort
 import jwt
 import os
+from werkzeug.utils import secure_filename
 from pathlib import Path
+from webapp import login_required
 app = Flask(__name__)
 app.secret_key = b'0293jr i(UHoiawu hft923'
 app.jwt_secret_key = 'SecretKey'
@@ -9,77 +11,64 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'uploads')
 app.config.update(
     #SESSION_COOKIE_SECURE=True,
-    SESSION_COOKIE_HTTPONLY=True
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
 )
 app.upload_path = Path(os.path.join(APP_ROOT, 'uploads'))
+JWT_ALGORITHM = 'HS384'
 
-@app.route("/upload", methods = ['POST', 'GET'])
+@app.route("/upload", methods=['POST', 'GET'])
+#@login_required
 def upload():
     token = request.form['token']
     try:
         user = jwt.decode(token.encode(), app.jwt_secret_key, algorithm='HS256')
     except jwt.ExpiredSignatureError:
-        return "error"
-
-    if files_limit(session.get('current_user')):
-            message = True
-            return render_template('upload.html', message=message)
-    target = UPLOAD_FOLDER+"/"+session.get('current_user')+"/"
-    if not os.path.isdir(target):
-        os.mkdir(target)
-    for file in request.files.getlist("file"):
-        filename = file.filename
-        destination = "/".join([target, filename])
-        file.save(destination)
+        return abort(401)
+    user_path = app.upload_path.joinpath(user['user']).resolve()
+    files = [x.name for x in user_path.glob('**/*') if x.is_file()]
+    files_len = len(files)
+    if files_len >=5:
+        return redirect('http://127.0.0.1:5001/upload')
+    if 'file' not in request.files:
+        return redirect('http://127.0.0.1:5001/upload')
+    f = request.files['file']
+    filename = secure_filename(f.filename)
+    user_path.mkdir(parents=True, exist_ok=True)
+    q = user_path / filename
+    f.save(str(q))
+    print("YOUR FILE HAS BEEN ADDED")
     return redirect('http://127.0.0.1:5001/list')
 
+
 @app.route("/download/<string:token>")
+#@login_required
 def download(token):
     try:
         user = jwt.decode(token.encode(), app.jwt_secret_key, algorithm='HS256')
     except jwt.ExpiredSignatureError:
         return "error"
-
     user_path = app.upload_path.joinpath(user['user']).resolve()
     q = user_path / user['file']
     if q.exists():
         return send_from_directory(user_path, user['file'])
-
-@app.route('/download/<string:filename>')
-def send_file_to_user(filename):
-        filepath = UPLOAD_FOLDER+"/"+session.get('current_user')+"/"+filename
-        if os.path.isfile(filepath):
-            return send_file(filepath)
+    else:
+        os.abort(404)
 
 
-@app.route('/delete/<string:filename>')
-def delete(filename):
-        filepath = UPLOAD_FOLDER+"/"+session.get("current_user")+"/"+filename
-        if os.path.isfile(filepath):
-            os.remove(filepath)
-            return redirect('http://127.0.0.1:5001/list')
-
-def get_user_files(username):
-    filepath = UPLOAD_FOLDER+"/"+str(session.get('current_user'))
-    files=[]
-    for filename in os.listdir(filepath):
-        data=[]
-        data.append(filename)
-        data.append("/download/"+filename)
-        data.append(("/delete/"+filename))
-        files.append(data)
-    return files
-
-
-def files_limit(username):
-    if not does_users_dir_exists(session.get('current_user')):
-        create_user_dir(session.get('current_user'))
-        return False
-    filepath = UPLOAD_FOLDER+"/"+str(session.get('current_user'))
-    counter=0
-    for filename in os.listdir(filepath):
-        counter = counter+1
-    return counter >=5
+@app.route('/delete/<string:token>')
+def delete(token):
+    try:
+        user = jwt.decode(token.encode(), app.jwt_secret_key, algorithm='HS256')
+    except jwt.ExpiredSignatureError:
+        return os.abort(401)
+    user_path = app.upload_path.joinpath(user['user']).resolve()
+    q = user_path / user['file']
+    if q.exists():
+        os.remove(q)
+        return redirect('http://127.0.0.1:5001/list')
+    else:
+        os.abort(404)
 
 
 def does_users_dir_exists(username):
--- webapp.py
+++ webapp.py
@@ -1,5 +1,6 @@
-from flask import Flask, session, render_template, redirect, request, json, url_for
+from flask import Flask, session, render_template, redirect, request, json, url_for, abort
 import os
+from functools import wraps
 import uuid
 import redis as redis
 import datetime
@@ -14,9 +15,24 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'uploads')
 app.config.update(
     #SESSION_COOKIE_SECURE=True,
-    SESSION_COOKIE_HTTPONLY=True
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+    SESSION_TYPE='redis'
 )
 app.upload_path = Path(os.path.join(APP_ROOT, 'uploads'))
+
+
+def login_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if 'current_user' not in session:
+            abort(401)
+        if redis.get(session['current_user']) is None:
+            abort(401)
+        return f(*args, **kwargs)
+    return wrapper
+
+
 with open('data.json') as jdata:
     data = json.load(jdata)
     app.users = data['users']
@@ -26,7 +42,7 @@ with open('data.json') as jdata:
 def home():
     if not session.get('current_user'):
         return redirect(url_for('login'))
-    return render_template('upload.html')
+    return redirect(url_for('file_add'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -37,11 +53,13 @@ def login():
         for user in data['users']:
             if request.form['username'] == user['username'] and request.form['password'] == user['password']:
                 current = user
+                session['logged_in'] = True
                 break
         if current:
             sid = str(uuid.uuid4())
             session['current_user'] = sid
-            redis.set(session['current_user'], user['username'], ex=50)
+            session['logged_in'] = True
+            redis.set(session['current_user'], user['username'], ex=300)
             return redirect(url_for('home'))
         else:
             error = "Invalid Credentials. Please try again."
@@ -51,23 +69,38 @@ def login():
 
 @app.route('/logout')
 def logout():
+    #session['current_user'] = None
+    session['logged_in'] = None
     redis.delete(session['current_user'])
     session.pop('current_user', None)
+    session.clear()
     return redirect(url_for('login'))
 
-@app.route("/list")
+
+@app.route("/list", methods=['GET', 'POST'])
+@login_required
 def list():
-    files = get_user_files(session['current_user'])
-    return render_template('list.html', files=files)
+    redis.expire(session['current_user'], time=300)
+    user_path = app.upload_path.joinpath(redis.get(session['current_user']).decode('utf-8')).resolve()
+    files = []
+    for filename in os.listdir(str(user_path)):
+        data = []
+        data.append(filename)
+        data.append("/download/" + filename)
+        data.append(("/delete/" + filename))
+        files.append(data)
+
+    tokens = {}
+    return render_template('list.html', user=redis.get(session['current_user']).decode('utf-8'), files=files, tokens=tokens)
 
 
 @app.route('/upload', methods=['GET', 'POST'])
 def file_add():
-    redis.expire(session['current_user'], time=50)
+    #redis.expire(session['current_user'], time=50)
     user_path = app.upload_path.joinpath(redis.get(session['current_user']).decode()).resolve()
     files = [x.name for x in user_path.glob('**/*') if x.is_file()]
     files_len = len(files)
-    token = creating_token("allow", 240).decode('utf-8')
+    token = creating_token("allow", 3000).decode('utf-8')
     return render_template('upload.html', files_len=files_len, token=token)
 
 
@@ -78,16 +111,7 @@ def creating_token(object, expiration):
     return jwt.encode(payload, app.jwt_secret_key, algorithm='HS256')
 
 
-def get_user_files(username):
-    filepath = UPLOAD_FOLDER+"/" +str(session.get('current_user'))
-    files = []
-    for filename in os.listdir(filepath):
-        data=[]
-        data.append(filename)
-        data.append("http://127.0.0.1:5002/download/"+filename)
-        data.append(("http://127.0.0.1:5002/delete/"+filename))
-        files.append(data)
-    return files
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
